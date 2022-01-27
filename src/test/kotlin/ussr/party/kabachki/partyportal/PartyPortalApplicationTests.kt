0a60722efce7eac92b8b28cd1464fee20129375d
vevteev <vevteev@neoflex.ru>
2022-01-27 17:17:51 +0300

    add about index page. Closes #7

---
+++ src/test/kotlin/ussr/party/kabachki/partyportal/PartyPortalApplicationTests.kt
@@ -0,0 +1,12 @@
+package ussr.party.kabachki.partyportal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PartyPortalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
