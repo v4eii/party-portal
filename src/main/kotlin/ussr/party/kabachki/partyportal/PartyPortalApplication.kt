176515a4f4a4c9b81858fc1220a0a94562c16208
vevteev <vevteev@neoflex.ru>
2022-01-27 15:11:11 +0300

    init project

---
+++ src/main/kotlin/ussr/party/kabachki/partyportal/PartyPortalApplication.kt
@@ -0,0 +1,11 @@
+package ussr.party.kabachki.partyportal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PartyPortalApplication
+
+fun main(args: Array<String>) {
+    runApplication<PartyPortalApplication>(*args)
+}
